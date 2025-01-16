c1ab43dac560523233e4120ddf9755f1c4b0da0f
╚»«╝ El'XiaoYinDeHioa ╚»«╝ <budierik457@gmail.com>
2025-01-16 10:31:49 +0700

    Update and rename twrp_x6827.mk to pbVictoria_x6827.mk

--- pbVictoria_x6827.mk
+++ pbVictoria_x6827.mk
@@ -13,12 +13,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 # Inherit from X6827 device
 $(call inherit-product, device/infinix/x6827/device.mk)
 
-# Inherit some common twrp stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+# Inherit some common pb stuff.
+$(call inherit-product, vendor/pbVictoria/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := x6827
-PRODUCT_NAME := twrp_x6827
+PRODUCT_NAME := pbVictoria_x6827
 PRODUCT_BRAND := Infinix
 PRODUCT_MODEL := Infinix HOT 20S
 PRODUCT_MANUFACTURER := infinix
